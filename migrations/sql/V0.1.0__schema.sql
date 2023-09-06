074350d77e3888a2b2c02c7c734081f80cfa9a65
dariogguillen <guillendario@gmail.com>
2023-09-06 15:43:48 -0600

    feat: base sql schema added

--- migrations/sql/V0.1.0__schema.sql
+++ migrations/sql/V0.1.0__schema.sql
@@ -4,7 +4,7 @@ CREATE EXTENSION pgcrypto;
 CREATE TABLE IF NOT EXISTS receipt_processor(
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     retailer VARCHAR(128) NOT NULL,
-    purchase_date TIMESTAMP WITH TIME ZONE NOT NULL,
+    purchase_date DATE NOT NULL,
     purchase_time VARCHAR(64),
     items JSON NOT NULL,
     total VARCHAR(63)
